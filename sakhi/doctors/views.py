ca819bfece0e72e3d0534e6b1d6d4e6773ef4f00
Sanskar <sanskar06march@gmail.com>
2024-10-22 22:30:27 +0530

    wrote doctors backend approx.

--- sakhi/doctors/views.py
+++ sakhi/doctors/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny
+from .serializers import GetAllDocsSerializer
+from .models import DoctorCategory, Doctors
+     
+class GetDoctors(generics.ListAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = GetAllDocsSerializer
 
-# Create your views here.
+    def get_queryset(self):
+        c = self.request.query_params.get('location', None)
+        category = self.request.query_params.get('category', None)
+        queryset = Doctors.objects.all()
+
+        if c:
+            c_lower = c.lower()
+            queryset = queryset.filter(cities__contains=[c_lower])
+
+            if not queryset.exists():
+                return Doctors.objects.none()
+
+        if category:
+            try:
+                category_instance = DoctorCategory.objects.get(name=category)
+                queryset = queryset.filter(category=category_instance)
+            except DoctorCategory.DoesNotExist:
+                return Doctors.objects.none()
+
+        return queryset.order_by('-rating')
+
+    def get(self, request, *args, **kwargs):
+        try:
+            queryset = self.get_queryset()
+            serializer = self.get_serializer(queryset, many=True)
+            return Response({
+                'message': 'Data fetched successfully!',
+                'data': serializer.data,
+                'status': 200,
+                'status_text': 'ok'
+            }, status=200)
+        except Exception as e:
+            return Response({
+                'message': 'Error!',
+                'error': str(e),
+                'status': 400,
+                'status_text': 'error'
+            }, status=400)
